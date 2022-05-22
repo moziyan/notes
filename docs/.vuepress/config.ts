7a4da39314d5960e5926da9a32b7e91db9282e70
lijiafeng <519982896@qq.com>
2022-05-22 17:25:13 +0800

    deploy

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,6 +3,9 @@ import {defineConfig} from "vuepress/config";
 export default defineConfig({
     title: "我的笔记本",
     description: "记下想记住的",
+    themeConfig: {
+        repo: 'moziyan/notes',
+    },
     plugins: [
         ['@vuepress/back-to-top'],
         ['@vuepress/medium-zoom']
