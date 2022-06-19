df5b1b6bc9b81e2db4fce41c241608d0e111d6d0
lijiafeng <519982896@qq.com>
2022-06-19 17:07:33 +0800

    add doc

--- docs/.vuepress/config/nav.ts
+++ docs/.vuepress/config/nav.ts
@@ -3,14 +3,14 @@ import {NavItem} from "vuepress/config";
 const nav: Array<NavItem> = [
     {
         text: "前端技术", items: [
-            {text: "面试", link: "/frontend-tech/interview/"},
+            {text: "面试题", link: "/frontend-tech/interview/"},
             {text: "React", link: "/frontend-tech/react/"},
             {text: "Vue", link: "/frontend-tech/vue/"}
         ]
     },
     {
         text: "后端技术", items: [
-            {text: "面试", link: "/backend-tech/interview/"},
+            {text: "面试题", link: "/backend-tech/interview/"},
             {text: "XXL-JOB", link: "/backend-tech/xxl-job/"},
             {text: "MySQL", link: "/backend-tech/mysql/"},
             {text: "Kafka", link: "/backend-tech/kafka/"},
